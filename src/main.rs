c1717365abc448ce3773d162c6ba3ba58552659b
hammurapi <3584715+hammurapi@users.noreply.github.com>
2025-08-04 17:20:29 +0200

    Adding 2D camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use bevy::prelude::*;
 
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
+
+#[derive(Component)]
+struct Paddle {
+    move_up: KeyCode,
+    move_down: KeyCode,
+    
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_systems(Startup, setup_camera);
+    app.run();
 }
